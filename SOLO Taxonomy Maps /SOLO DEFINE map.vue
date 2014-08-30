3d286c8f8b6cbfb515d781cb1e4dba291da7d55f
kubke <mf.kubke@gmail.com>
2014-08-30 19:38:50 +1200

    Create repository
    Testing uploading concept maps

---
+++ SOLO Taxonomy Maps /SOLO DEFINE map.vue
@@ -0,0 +1,222 @@
+<!-- Tufts VUE 3.2.2 concept-map (SOLO DEFINE map.vue) 2014-08-30 -->
+<!-- Tufts VUE: http://vue.tufts.edu/ -->
+<!-- Do Not Remove: VUE mapping @version(1.1) jar:file:/Applications/VUE.app/Contents/VUE.jar!/tufts/vue/resources/lw_mapping_1_1.xml -->
+<!-- Do Not Remove: Saved date Sat Aug 30 13:13:55 NZST 2014 by kubke on platform Mac OS X 10.9.4 in JVM 1.6.0_65-b14-462-11M4609 -->
+<!-- Do Not Remove: Saving version @(#)VUE: built May 23 2013 at 2146 by tomadm on Linux 2.6.18-348.2.1.el5 i386 JVM 1.7.0_21-b11(bits=32) -->
+<?xml version="1.0" encoding="US-ASCII"?>
+<LW-MAP xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xsi:noNamespaceSchemaLocation="none" ID="0"
+    label="SOLO DEFINE map.vue" created="1409360738271" x="0.0" y="0.0"
+    width="1.4E-45" height="1.4E-45" strokeWidth="0.0" autoSized="false">
+    <resource referenceCreated="1409361235289"
+        spec="/Users/kubke/Desktop/SOLO Taxonomy/SOLO DEFINE map.vue"
+        type="1" xsi:type="URLResource">
+        <title>SOLO DEFINE map.vue</title>
+        <property key="File" value="/Users/kubke/Desktop/SOLO Taxonomy/SOLO DEFINE map.vue"/>
+    </resource>
+    <fillColor>#FFFFFF</fillColor>
+    <strokeColor>#404040</strokeColor>
+    <textColor>#000000</textColor>
+    <font>SansSerif-plain-14</font>
+    <URIString>http://vue.tufts.edu/rdf/resource/2479cd67c0a8010601487e30827d0a2f</URIString>
+    <child ID="8" layerID="1" created="1409360831246" x="236.0" y="73.0"
+        width="472.0" height="326.0" strokeWidth="1.0" autoSized="false" xsi:type="node">
+        <fillColor>#EEEEEE</fillColor>
+        <strokeColor>#000000</strokeColor>
+        <textColor>#000000</textColor>
+        <font>Arial-plain-12</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/2479cd6ac0a8010601487e30a352caf6</URIString>
+        <child ID="11" label="Not relevant" created="1409360975503"
+            x="5.0" y="22.0" width="150.0" height="30.0"
+            strokeWidth="0.0" autoSized="false" xsi:type="text">
+            <strokeColor>#404040</strokeColor>
+            <textColor>#000000</textColor>
+            <font>SansSerif-plain-14</font>
+            <URIString>http://vue.tufts.edu/rdf/resource/2479cd6ac0a8010601487e3068167676</URIString>
+            <richText>&lt;html&gt;
+  &lt;head color="#000000" style="color: #000000"&gt;
+    &lt;style type="text/css"&gt;
+      &lt;!--
+        body { font-family: Arial; font-size: 12; margin-bottom: 0px; color: #000000; margin-left: 0px; margin-right: 0px; margin-top: 0px }
+        ol { vertical-align: middle; font-family: Arial; font-size: 12; margin-left: 30; list-style-position: outside; margin-top: 6 }
+        p { margin-bottom: 0; color: #000000; margin-left: 0; margin-right: 0; margin-top: 0 }
+        ul { vertical-align: middle; font-family: Arial; font-size: 12; list-style-position: outside; margin-left: 30; margin-top: 6 }
+      --&gt;
+    &lt;/style&gt;
+    
+  &lt;/head&gt;
+  &lt;body&gt;
+    &lt;p color="#000000" style="color: #000000"&gt;
+      Not relevant
+    &lt;/p&gt;
+  &lt;/body&gt;
+&lt;/html&gt;
+</richText>
+            <label>Not relevant</label>
+        </child>
+        <shape xsi:type="rectangle"/>
+    </child>
+    <child ID="7" layerID="1" created="1409360814737" x="286.0"
+        y="132.0" width="353.0" height="215.0" strokeWidth="1.0"
+        autoSized="false" xsi:type="node">
+        <fillColor>#EEEEEE</fillColor>
+        <strokeColor>#000000</strokeColor>
+        <textColor>#000000</textColor>
+        <font>Arial-plain-12</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/2479cd6cc0a8010601487e302c55a4ca</URIString>
+        <child ID="12" label="Relevant" created="1409360995967" x="5.0"
+            y="22.0" width="150.0" height="30.0" strokeWidth="0.0"
+            autoSized="false" xsi:type="text">
+            <strokeColor>#404040</strokeColor>
+            <textColor>#000000</textColor>
+            <font>SansSerif-plain-14</font>
+            <URIString>http://vue.tufts.edu/rdf/resource/2479cd6cc0a8010601487e3046da57ee</URIString>
+            <richText>&lt;html&gt;
+  &lt;head color="#000000" style="color: #000000"&gt;
+    &lt;style type="text/css"&gt;
+      &lt;!--
+        body { font-family: Arial; font-size: 12; margin-bottom: 0px; color: #000000; margin-left: 0px; margin-right: 0px; margin-top: 0px }
+        ol { vertical-align: middle; font-family: Arial; font-size: 12; margin-left: 30; list-style-position: outside; margin-top: 6 }
+        p { margin-bottom: 0; color: #000000; margin-left: 0; margin-right: 0; margin-top: 0 }
+        ul { vertical-align: middle; font-family: Arial; font-size: 12; list-style-position: outside; margin-left: 30; margin-top: 6 }
+      --&gt;
+    &lt;/style&gt;
+    
+  &lt;/head&gt;
+  &lt;body&gt;
+    &lt;p color="#000000" style="color: #000000"&gt;
+      Relevant
+    &lt;/p&gt;
+    &lt;p color="#000000" style="color: #000000"&gt;
+      
+    &lt;/p&gt;
+  &lt;/body&gt;
+&lt;/html&gt;
+</richText>
+            <label>Relevant</label>
+        </child>
+        <shape xsi:type="rectangle"/>
+    </child>
+    <child ID="6" layerID="1" created="1409360790024" x="347.0"
+        y="196.0" width="210.0" height="96.0" strokeWidth="1.0"
+        autoSized="false" xsi:type="node">
+        <fillColor>#EEEEEE</fillColor>
+        <strokeColor>#000000</strokeColor>
+        <textColor>#000000</textColor>
+        <font>Arial-plain-12</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/2479cd6dc0a8010601487e30d2829da9</URIString>
+        <shape xsi:type="rectangle"/>
+    </child>
+    <child ID="13" layerID="1" created="1409361041413" x="236.0"
+        y="412.0" width="470.0" height="86.0" strokeWidth="1.0"
+        autoSized="false" xsi:type="node">
+        <fillColor>#EEEEEE</fillColor>
+        <strokeColor>#000000</strokeColor>
+        <textColor>#000000</textColor>
+        <font>Arial-plain-12</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/2479cd6ec0a8010601487e303828eeae</URIString>
+        <child ID="14"
+            label="Overall:.......................&#xa;    &#xa;    &#xa;      &#xa;    &#xa;    &#xa;      because .........................                       &#xa;      because.........................."
+            created="1409361112818" x="5.0" y="22.0" width="453.0"
+            height="58.0" strokeWidth="0.0" autoSized="false" xsi:type="text">
+            <strokeColor>#404040</strokeColor>
+            <textColor>#000000</textColor>
+            <font>SansSerif-plain-14</font>
+            <URIString>http://vue.tufts.edu/rdf/resource/2479cd6fc0a8010601487e30cf853abb</URIString>
+            <richText>&lt;html&gt;
+  &lt;head color="#000000" style="color: #000000"&gt;
+    &lt;style type="text/css"&gt;
+      &lt;!--
+        body { font-family: Arial; font-size: 12; margin-bottom: 0px; color: #000000; margin-left: 0px; margin-right: 0px; margin-top: 0px }
+        ol { vertical-align: middle; font-family: Arial; font-size: 12; margin-left: 30; list-style-position: outside; margin-top: 6 }
+        p { margin-bottom: 0; color: #000000; margin-left: 0; margin-right: 0; margin-top: 0 }
+        ul { vertical-align: middle; font-family: Arial; font-size: 12; list-style-position: outside; margin-left: 30; margin-top: 6 }
+      --&gt;
+    &lt;/style&gt;
+    
+  &lt;/head&gt;
+  &lt;body&gt;
+    &lt;p color="#000000" style="color: #000000; text-align: left"&gt;
+      Overall:.......................
+    &lt;/p&gt;
+    &lt;p color="#000000" style="color: #000000; text-align: left"&gt;
+      
+    &lt;/p&gt;
+    &lt;p color="#000000" style="color: #000000; text-align: left"&gt;
+      because .........................                       
+      because..........................
+    &lt;/p&gt;
+  &lt;/body&gt;
+&lt;/html&gt;
+</richText>
+            <label>Overall:.......................
+    
+    
+      
+    
+    
+      because .........................                       
+      because..........................</label>
+        </child>
+        <shape xsi:type="rectangle"/>
+    </child>
+    <layer ID="1" label="Layer 1" created="1409360738280" x="0.0"
+        y="0.0" width="1.4E-45" height="1.4E-45" strokeWidth="0.0" autoSized="false">
+        <URIString>http://vue.tufts.edu/rdf/resource/2479cd70c0a8010601487e3002faa50b</URIString>
+    </layer>
+    <userZoom>1.0</userZoom>
+    <userOrigin x="-59.0" y="-14.0"/>
+    <presentationBackground>#202020</presentationBackground>
+    <PathwayList currentPathway="0" revealerIndex="-1">
+        <pathway ID="0" label="Untitled Pathway" created="1409360738269"
+            x="0.0" y="0.0" width="1.4E-45" height="1.4E-45"
+            strokeWidth="0.0" autoSized="false" currentIndex="-1" open="true">
+            <strokeColor>#B3993333</strokeColor>
+            <textColor>#000000</textColor>
+            <font>SansSerif-plain-14</font>
+            <URIString>http://vue.tufts.edu/rdf/resource/2479cd74c0a8010601487e3085e6f5f6</URIString>
+            <masterSlide ID="2" created="1409360738305" x="0.0" y="0.0"
+                width="800.0" height="600.0" locked="true"
+                strokeWidth="0.0" autoSized="false">
+                <fillColor>#000000</fillColor>
+                <strokeColor>#404040</strokeColor>
+                <textColor>#000000</textColor>
+                <font>SansSerif-plain-14</font>
+                <URIString>http://vue.tufts.edu/rdf/resource/2479cd75c0a8010601487e301a66ec67</URIString>
+                <titleStyle ID="3" label="Header"
+                    created="1409360738431" x="341.0" y="175.0"
+                    width="118.0" height="50.0" strokeWidth="0.0"
+                    autoSized="true" isStyle="true" xsi:type="node">
+                    <strokeColor>#404040</strokeColor>
+                    <textColor>#FFFFFF</textColor>
+                    <font>Gill Sans-plain-36</font>
+                    <URIString>http://vue.tufts.edu/rdf/resource/2479cd76c0a8010601487e30873bf2b7</URIString>
+                    <shape xsi:type="rectangle"/>
+                </titleStyle>
+                <textStyle ID="4" label="Slide Text"
+                    created="1409360738433" x="349.5" y="282.5"
+                    width="101.0" height="35.0" strokeWidth="0.0"
+                    autoSized="true" isStyle="true" xsi:type="node">
+                    <strokeColor>#404040</strokeColor>
+                    <textColor>#FFFFFF</textColor>
+                    <font>Gill Sans-plain-22</font>
+                    <URIString>http://vue.tufts.edu/rdf/resource/2479cd76c0a8010601487e30c5bbb6b7</URIString>
+                    <shape xsi:type="rectangle"/>
+                </textStyle>
+                <linkStyle ID="5" label="Links" created="1409360738435"
+                    x="375.5" y="385.0" width="49.0" height="30.0"
+                    strokeWidth="0.0" autoSized="true" isStyle="true" xsi:type="node">
+                    <strokeColor>#404040</strokeColor>
+                    <textColor>#B3BFE3</textColor>
+                    <font>Gill Sans-plain-18</font>
+                    <URIString>http://vue.tufts.edu/rdf/resource/2479cd77c0a8010601487e30bac5c0be</URIString>
+                    <shape xsi:type="rectangle"/>
+                </linkStyle>
+            </masterSlide>
+        </pathway>
+    </PathwayList>
+    <date>2014-08-30</date>
+    <modelVersion>6</modelVersion>
+    <saveLocation>/Users/kubke/Desktop/SOLO Taxonomy</saveLocation>
+    <saveFile>/Users/kubke/Desktop/SOLO Taxonomy/SOLO DEFINE map.vue</saveFile>
+</LW-MAP>
